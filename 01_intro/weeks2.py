6d427b1162625796f2030b8887ebf689de4c34ca
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-05-11 18:22:04 +0200

    modyfy

--- 01_intro/weeks2.py
+++ 01_intro/weeks2.py
@@ -2,6 +2,6 @@
 learning_hours = float(input('Ile godzin trwa kurs? '))
 my_time = float(input('Ile godzin w tygodniu poświęcisz? '))
 full_cours = learning_hours / my_time
-resoult2 = round(full_cours, 0)
+resoult2 = round(full_cours, 1)
 
 print('Zakładając, że poświęcisz', my_time, 'godzin w tygodniu na kurs który trwa', learning_hours, 'godzin, ukończysz go w:' , resoult2, 'tygodni')
