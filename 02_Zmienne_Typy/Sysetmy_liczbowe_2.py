8671df0fe8845209132417a15a96c1c74bc23cef
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-05-15 22:49:56 +0200

    02_Zmienne_Typy/

--- 02_Zmienne_Typy/Sysetmy_liczbowe_2.py
+++ 02_Zmienne_Typy/Sysetmy_liczbowe_2.py
@@ -1,7 +1,10 @@
 bin_num = "1001111"
+decimal = (1 * 2 ** 0) + (1 * 2 ** 1) + (1 * 2 ** 2) + (1 * 2 ** 3) + (0 * 2 ** 4) + (0 * 2 ** 5) + (1 * 2 ** 6)
+print(decimal)
+
+
+
 # decimal = int(bin_num, 2)
 # print('Liczba dziesiętna z binarnej 1001111 wynosi:', str(decimal))
 # wynik to 79
 
-# decimal = (bin_num[:6] * 2 ** 0) + (bin_num[:5] * 2 ** 1)
-# print(decimal)
