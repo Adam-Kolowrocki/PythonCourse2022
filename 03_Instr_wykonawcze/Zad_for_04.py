a3df99f39f033aee4f7fedb3232d0867507c2ef2
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-05-22 19:13:52 +0200

    03_Instr_wykonawcze/

--- 03_Instr_wykonawcze/Zad_for_04.py
+++ 03_Instr_wykonawcze/Zad_for_04.py
@@ -1,15 +1,23 @@
 # Napisz program, który wyświetli kolejne wyniki dla silni liczby naturalnej N
 # (N podane przez użytkownika, ale nie większe niż 8).
-# NIEZROBIONE
 
 nat = int(input('Podaj liczbę naturalną z zakresu od 1 do 8:'))
-print(f'{nat}! =')
+i = 1
+s = 1
+if nat == 0:
+    print('0! = 1')
+elif nat == 1:
+    print('1! = 1')
+else:
+    print(f'{nat}! =')
+for n in range(1, nat):
+    print(n, end=' * ')
+while i <= nat:
+    s *= i
+    i += 1
+print(nat, '=', s)
+print(f'Silnia z {nat} wynosi {s}')
 
-for n in range(2, nat):
-    silnia(n *= )
-
-for i in range(1, nat):
-    print(i, end=' * ')
 
 
 
