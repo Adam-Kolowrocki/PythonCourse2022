7ad6835d0b9b36b15cd7dddc01e465800c982dea
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-06-23 00:10:02 +0200

    04_Kolekcje/

--- 10_Algorytmy/Zad_06_algorytmy.py
+++ 10_Algorytmy/Zad_06_algorytmy.py
@@ -1,12 +1,15 @@
 # Poznaj trójkąt Pascala. Napisz kod, który wyświetla trójkąt Pascala dla podanego N.
 def data_input():
-
+    user_n = int(input(f'Podaj "N" -> '))
+    return user_n
 
 
 def pascal_triangle():
+    n = data_input()
 
 
 def main():
+    print(f'Program zwracający trójkąt Pascala dla podanej wartości "N"')
     pascal_triangle()
 
 
