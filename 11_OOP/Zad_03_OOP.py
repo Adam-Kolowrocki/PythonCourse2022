8551ec718e5e04d7fe3e255eecc6587587791939
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-06-23 19:21:27 +0200

    04_Kolekcje/

--- 11_OOP/Zad_03_OOP.py
+++ 11_OOP/Zad_03_OOP.py
@@ -4,3 +4,44 @@
 # dodanie elementu do kolejki (put), wyjęcie elementu z kolejki (get).
 #
 # Utwórz kilka obiektów klasy Queue z różnymi parametrami.
+
+kolejka = {
+  'name': 'Kolejka',
+  'length': '10',
+  'type_1': 'string'
+}
+
+
+class Queue():
+    def __init__(self, fifo):
+        self.fifo = fifo
+
+
+    def wyświetl(self):
+        print('Queue:', self.fifo)
+
+
+    def sprawdź(self):
+        return  len(self.fifo) == 0
+
+    def dodanie(self, item):
+        self.fifo.append(item)
+
+
+    def wyjęcie(self):
+        return self.fifo.pop(0)
+
+
+def main():
+    q = Queue([3, 2, 7, 9, 'txt'])
+    q.wyświetl()
+    print(q.sprawdź())
+    q.dodanie('Adam')
+    q.wyświetl()
+    print(q.fifo)
+    print(q.wyjęcie())
+    q.wyświetl()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 11_OOP/Zad_Dodatkowe_OOP.py
@@ -0,0 +1,45 @@
+# Napisz klasę Stack
+# get element - wyciągnij element ze stosul
+# show - wyświetl cały stos
+# push - dodaj element do stosu
+
+stos = {
+  'name': 'Stos',
+  'length': '10',
+  'type_1': 'string'
+}
+
+
+class Stos():
+    def __init__(self, lifo):
+        self.lifo = lifo
+
+
+    def wyświetl(self):
+        print('Stos:', self.lifo)
+
+
+    def sprawdź(self):
+        return len(self.lifo) == 0
+
+    def dodanie(self, item):
+        self.lifo.append(item)
+
+
+    def wyjęcie(self):
+        return self.lifo.pop(0)
+
+
+def main():
+    s = Stos([3, 2, 7, 9, 'txt'])
+    s.wyświetl()
+    print(s.sprawdź())
+    s.dodanie('Adam')
+    s.wyświetl()
+    print(s.lifo)
+    print(s.wyjęcie())
+    s.wyświetl()
+
+
+if __name__ == "__main__":
+    main()
