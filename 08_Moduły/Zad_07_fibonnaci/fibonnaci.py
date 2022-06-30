4de1fc8514052ee4d24153d909c8a49c6b0d558f
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-06-30 23:03:24 +0200

    04_Kolekcje/

--- 08_Moduły/Zad_07_fibonnaci/fibonnaci.py
+++ 08_Moduły/Zad_07_fibonnaci/fibonnaci.py
@@ -2,13 +2,22 @@ n = 5
 
 
 def fibo(n):
-    sequence = {'F0': 0, 'F1': 1}
-    for i in range(2, n + 1):
-        for key, val in sequence:
-            key =
+    if n == 0:
+        sequence = {'F0': 0}
+        return sequence
+    elif n == 1:
+        sequence = {'F0': 0, 'F1': 1}
+        return sequence
+    elif n > 1:
+        sequence = {'F0': 0, 'F1': 1}
+        for i in range(2, n + 1):
+            for key, val in sequence:
+                key = 'F'[i], val = F[i -1] + F[i - 2]
+
+
         sequence.append((i - 1) + (i - 2))       #  F{i}=(i + (i - 1))
         print(sequence)
-    return sequence
+        return sequence
 
 
 fibo(n)
