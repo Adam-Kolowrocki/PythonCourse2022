52bf0e48941cfe25335cc6b3fee5ce02299ac0e5
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-06-30 15:23:39 +0200

    04_Kolekcje/

--- 08_Moduły/bmi.py
+++ 08_Moduły/bmi.py
@@ -1,11 +1,32 @@
-# obliczanie BMI podejście trzecie - trening
+# Moduł do obliczania BMI.
+def user_data():
+    print('Witaj, to jest prosty programik do obliczania Twojego BMI.')
+    print()
+    masa = float(input('Podaj proszę swoją masę w kg, używając "." jako separatora: '))
+    wzrost = float(input('Podaj proszę swój wzrost w metrach, używając "." jako separatora: '))
+    return masa, wzrost
+
 
 def bmi_calc():
+    masa, wzrost = user_data()
     bmi = (masa / wzrost ** 2)
     print('Twoje BMI wynosi: ', round(bmi, 1))
+    return bmi
+
+
+def user_suggestion():
+    bmi = bmi_calc()
     if bmi < 18.5:
-        return niedowaga
-    if bmi > 18.5 and bmi < 24.9:
-        return waga_normalna
-    if bmi > 24.9:
-        return nadwaga
+        print('Twoje BMi jest zbyt małe, postaraj się jeść więcej chipsów!!!')
+    elif 18.5 < bmi < 24.9:
+        print('Twoje BMI jest idealne, żyj zdrowo.')
+    elif bmi > 24.9:
+        print('Twoje BMI jest zbyt wysokie, odstaw chipsy i piwo!!!')
+
+
+def main():
+    user_suggestion()
+
+
+if __name__ == "__main__":
+    main()
