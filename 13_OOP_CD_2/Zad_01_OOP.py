5cf87bae3139141d7feca204cba8fabb8e55bb94
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-07-05 21:50:17 +0200

    04_Kolekcje/

--- 13_OOP_CD_2/Zad_01_OOP.py
+++ 13_OOP_CD_2/Zad_01_OOP.py
@@ -1,6 +1,9 @@
 # Utwórz dekorator @uppercase_decorator,
 # który przyjmuje dowolną funkcję zawracającą łańcuch znaków i zwracający ten sam tekst
 # zmieniony na wielkie litery.
+# Utwórz dekorator @timepassed mierzący czas działania dowolnej funkcji.
+import time
+
 
 def uppercase_decorator(text_func):
     def upper_letter():
@@ -8,6 +11,11 @@ def uppercase_decorator(text_func):
         return text_do_zmiany.upper()
 
     return upper_letter
+
+
+def timepassed(func):
+    f_time = time.time()
+    return f_time
 
 
 @uppercase_decorator
